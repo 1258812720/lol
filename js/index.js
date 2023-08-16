9476bd0f06d00caa6d025d6cdb145a62bf1b9072
夜的第七章 <1258812720@qq.com>
2023-08-16 22:22:29 +0800

    引入第三方库

--- js/index.js
+++ js/index.js
@@ -23,6 +23,8 @@ $(document).ready(function () {
     }
     set_attr(page, $("img"));
     const menu = $(".menu_list");
+    var first = true;
+    const content = $(".menu-wrapper").eq(0)
     list.menu.forEach(item => {
         let li = $(`<li class='menu_items'> 
             <span class="menu_dazi"> ${item.txt.toLocaleUpperCase()} </span>
@@ -30,4 +32,85 @@ $(document).ready(function () {
         </li>`);
         menu.append(li);
     });
+    menu.on("mouseenter", () => {
+        content.parent(".menu_content").css({
+            display: "block"
+        }).addClass("fade-in");
+        if (first) {
+            first = !first;
+            menu_init();
+        }
+    });
+    content.on("mouseleave", () => {
+        content.parent(".menu_content").removeClass("fade-in").css({
+            display: "none"
+        });
+    });
+
+    // 菜单栏
+    function menu_init() {
+        list.menu2.forEach(item => {
+            let ul = null;
+            if (item && item.length > 0) {
+                ul = $("<ul class='menu-ul'></ul>");
+                item.forEach(child => {
+                    let li = $(`<li class="menu-child">
+                        ${child.icon ? '<i class="menu-icon " style="--icon-x:' + child.icon[0] + 'px;--icon-y:' + child.icon[1] + 'px;"></i>' : ''}
+                        <span> ${child.txt} </span>
+                    </li>`);
+                    ul.append(li)
+                });
+            }
+            if (ul) {
+                content.append(ul)
+            }
+        });
+    }
+
+    const content02 = {
+        init() {
+            this.swiper()
+        },
+        swiper() {
+            const tabs = list.swiper.tab,
+                data = list.swiper.data;
+            let d = $(".tab-btn");
+            tabs.forEach(i => {
+                var span = $("<span class='tab-btns'> " + [i] + " </span>");
+                d.append(span);
+            });
+            const wrapper = $(".swiper-wrapper").eq(0);
+            data.forEach(i => {
+                var items = $(`<div class='swiper-items'>
+                <img data='${i}' alt=" " />
+                </div>`);
+                wrapper.append(items);
+            });
+            const children = d.children("span");
+            var demo = new SimSwiper("#hero-swiper", {
+                autoplay: 3500,
+                loop: true,
+                touch:false,
+                lazy: {
+                    prop: "data"
+                },
+                change(ev){
+                    console.log(ev)
+                    children.eq(ev.index).addClass("btn-select").siblings("span")
+                    .removeClass("btn-select")
+                },
+                duration: 300,
+            });
+            children.on("mouseover",function(){
+                demo.stop();
+                demo.index=$(this).index();
+                demo.slide_to();
+                $(this).addClass("btn-select")
+            });
+            children.on("mouseout",function(){
+                demo.boot();
+            })
+        }
+    }
+    content02.init();
 });
--- js/lol.config.js
+++ js/lol.config.js
@@ -2,19 +2,45 @@
  * 数据配置脚本
  */
 const page = {
-    "top_bg":"https://img.crawler.qq.com/lolwebschool/0/JAutoCMS_LOLWeb_fc06683d2f6ff1b5535fdbbd533ef1ba/0",
-    "top_bg1":"https://img.crawler.qq.com/lolwebschool/0/JAutoCMS_LOLWeb_dd8dbf05bf73072a5fba4120bda9c192/0",
-    "txt_logo":"https://game.gtimg.cn/images/lol/v3/logo-public.png"
+    "top_bg": "https://img.crawler.qq.com/lolwebschool/0/JAutoCMS_LOLWeb_fc06683d2f6ff1b5535fdbbd533ef1ba/0",
+    "top_bg1": "https://img.crawler.qq.com/lolwebschool/0/JAutoCMS_LOLWeb_dd8dbf05bf73072a5fba4120bda9c192/0",
+    "txt_logo": "https://game.gtimg.cn/images/lol/v3/logo-public.png"
 }
+const sprite = "https://game.gtimg.cn/images/lol/v3/topfoot-spr.png";// 精灵图
 const list = {
-    menu:[
+    menu: [
         {
-            txt:"游戏资料",
-            txt_en:"game info"
+            txt: "游戏资料",
+            txt_en: "game info"
         },
         {
-            txt:"商城/合作",
-            txt_en:"STORe"
+            txt: "商城/合作",
+            txt_en: "STORe"
         }
-    ]
+    ],
+    menu2: [
+        [
+            {
+                txt: "游戏下载",
+                icon: [-347, -84]
+            },
+            {
+                txt: "新手只因"
+            }
+        ], [
+            {
+                txt: "点券充值",
+                icon: [-381, -65]
+            }
+        ]
+    ],
+    swiper: {
+        tab: ["十二周年战斗之夜", "斗魂觉醒", "神话级棋盘限时获取", "巅峰夺宝", "七夕限定悠米手办上线"],
+        data: ["https://ossweb-img.qq.com/upload/adw/image/977/20230808/76c29ef3289f10f3e43625ddc9eadba3.jpeg",
+            "https://ossweb-img.qq.com/upload/adw/image/977/20230804/153df95b4f999a6d1538da2209e98a52.jpeg",
+            "https://ossweb-img.qq.com/upload/adw/image/977/20230720/8db5db7da2a22b1347aa8474162a5610.jpeg",
+            "https://ossweb-img.qq.com/upload/adw/image/977/20230725/442b3db768eb10ab18775e131686100a.jpeg",
+            "https://ossweb-img.qq.com/upload/adw/image/977/20230810/2a72e0911d83a1ff2a9c05b1191e9e36.jpeg"
+        ]
+    }
 }
